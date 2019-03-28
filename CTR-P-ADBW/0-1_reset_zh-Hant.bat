9770e72a0f3d98d889c4467b957e1f5eb6d8bacf
Sun Daowen <sundaowen@gmail.com>
2019-03-29 00:21:44 +0800

    init CTR-P-ADBW

---
+++ CTR-P-ADBW/0-1_reset_zh-Hant.bat
@@ -0,0 +1,8 @@
+PUSHD "%~dp0"
+RD /S /Q cci
+RD /S /Q build
+DEL "Nintendogs + Cats - French Bulldog & New Friends (TWN) (zh-Hant).3ds"
+DEL patch.3ps
+DEL zh\icon.ico
+DEL zh-Hant_ADBWTOOLS.exe
+POPD
